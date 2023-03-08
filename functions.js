b951baf36e16d0cb2f44875011bf555f86a61f0d
pujasharma5431 <pujasharma5431@gmail.com>
2023-03-08 14:56:26 +0545

    Null

--- functions.js
+++ functions.js
@@ -1,6 +1,7 @@
 const functions={
     add:(num1, num2) =>
-       num1+num2+1
+       num1+num2+1,
+       isNull: ()=>null
     
 }
 module.exports=functions;
--- functions.test.js
+++ functions.test.js
@@ -2,6 +2,15 @@ const functions = require('./functions');
 test('Add 2+2 to equal 4',()=>{
     expect(functions.add(2,2)).toBe(5);
 });
+
+// Not to be
+
 test('Add 2+2 to equal not equals to 5',()=>{
     expect(functions.add(2,2)).not.toBe(6);
+});
+
+
+// toBeNull matches only null
+test('Should be nulll',()=>{
+    expect(functions.isNull()).toBeNull();
 });
